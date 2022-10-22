e220c10b1c8696490ff906107d8d276078571a38
ShaduDev <cohenjofir@gmail.com>
2022-10-22 16:31:00 +0800

    Changed release server ports to 38281

--- MultiWorldLib/Consts.cs
+++ MultiWorldLib/Consts.cs
@@ -6,7 +6,7 @@
 #if (DEBUG)
         public static readonly int DEFAULT_PORT = 38283;
 #else
-        public static readonly int DEFAULT_PORT = 38282;
+        public static readonly int DEFAULT_PORT = 38281;
 #endif
         public static readonly string PUBLIC_SERVER_URL = "18.189.16.129";
 
